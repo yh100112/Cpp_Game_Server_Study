b6b2c6796daaa18ffa75ad84e39a6a844eed970d
ejrdyd1001@gmail.com <ejrdyd1001@gmail.com>
2024-10-13 15:10:55 +0900

    멀티스레드 함수 사용

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -2,7 +2,34 @@
 #include <iostream>
 #include "CorePch.h"
 
+#include <thread>
+
+void HelloThread()
+{
+    cout << "Hello Thread" << endl;
+}
+
+void HelloThread2(int32 num)
+{
+    cout << num << endl;
+}
+
+// windows, linux 서버 둘 다에서 공용으로 사용되는 라이브러리를 사용하도록 하자!
 int main()
 {
-    HelloWorld();
+    vector<std::thread> v;
+
+    for (int32 i = 0; i < 10; i++)
+    {
+        v.push_back(std::thread(HelloThread2, i));
+    }
+
+    for (int32 i = 0; i < 10; i++)
+    {
+        if (v[i].joinable())
+            v[i].join();
+    }
+
+
+    cout << "Hello Main" << endl;
 }
