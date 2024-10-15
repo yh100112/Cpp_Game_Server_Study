d76db76f609a1697ec7861f92da2ec1ab9c6070e
ejrdyd1001@gmail.com <ejrdyd1001@gmail.com>
2024-10-15 22:18:28 +0900

    memery cash hit

---
+++ GameServer/memory_cash.cpp
@@ -0,0 +1,35 @@
+#include <atomic>
+#include "CorePch.h"
+#include <mutex>
+#include <windows.h>
+#include <future>
+#include "pch.h"
+#include <iostream>
+
+int32 buffer[10000][10000];
+
+int main() {
+	memset(buffer, 0, sizeof(buffer));
+
+	{
+		uint64 start = GetTickCount64();
+		int64 sum = 0;
+		for (int32 i = 0; i < 10000; i++)
+			for (int32 j = 0; j < 10000; j++)
+				sum += buffer[i][j];
+
+		uint64 end = GetTickCount64();
+		cout << "Elapsed Tick " << (end - start) << endl;
+	}
+	
+	{
+		uint64 start = GetTickCount64();
+		int64 sum = 0;
+		for (int32 i = 0; i < 10000; i++)
+			for (int32 j = 0; j < 10000; j++)
+				sum += buffer[j][i];
+
+		uint64 end = GetTickCount64();
+		cout << "Elapsed Tick " << (end - start) << endl;
+	}
+}
