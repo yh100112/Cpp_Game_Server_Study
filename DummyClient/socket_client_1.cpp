dccb2c11acb944d540532cc8b21b9a1b231c552c
김덕용 <yh100112@naver.com>
2024-10-28 07:04:14 +0900

    소켓 프로그래밍 구현

---
+++ DummyClient/socket_client_1.cpp
@@ -0,0 +1,70 @@
+﻿#include "pch.h"
+#include <iostream>
+
+#include <winsock2.h> // windows socket 프로그래밍에 필요
+#include <mswsock.h>  // 소켓 프로그래밍에 필요
+#include <ws2tcpip.h> // 소켓 프로그래밍에 필요
+#pragma comment(lib, "ws2_32.lib") // 속성에서 설정하기 귀찮으니 사용 라이브러리를 코드에 추가
+
+
+int main()
+{
+	// 윈도우 소켓 초기화 (ws2_32 라이브러리 초기화)
+	// 관련 정보가 wsaData에 채워짐
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
+
+	// ad : Address Family (AF_INET = IPv4, AF_INET6 = IPv6)
+	// type : TCP(SOCK_STREAM) vs UDP(SOCK_DGRAM)
+	// protocol : 0
+	// return : descriptor
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (clientSocket == INVALID_SOCKET)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Socket ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	// 연결할 목적지는? (IP주소 + Port) -> XX 아파트 YY 호
+	SOCKADDR_IN serverAddr; // IPv4
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	//serverAddr.sin_addr.s_addr = ::inet_addr("127.0.0.1"); // 구식 방식 ( deprecated )
+	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr); // 최신 방식
+
+	serverAddr.sin_port = ::htons(7777); // 80 : HTTP
+	// host to network short -> 호스트에서 네트워크 방식으로 바꿔주는 함수
+	// Little-Endian vs Big-Endian
+	// ex) 0x12345678 4바이트 정수
+	// low [0x78][0x56][0x34][0x12] high : little 방식
+	// low [0x12][0x34][0x56][0x78] high : big 방식    = network 표준
+
+	if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Connect ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	// ---------------------------
+	// 연결 성공! 이제부터 데이터 송수신 가능!
+
+	cout << "Connected To Server!" << endl;
+
+	while (true)
+	{
+		// TODO
+
+		this_thread::sleep_for(1s);
+	}
+
+	// ---------------------------
+
+	// 소켓 리소스 반환
+	::closesocket(clientSocket);
+
+	// 윈도우 소켓 종료
+	::WSACleanup();
+}
---
+++ GameServer/socket_server_1.cpp
@@ -0,0 +1,87 @@
+#include "pch.h"
+#include <iostream>
+#include "CorePch.h"
+#include <atomic>
+#include <mutex>
+#include <windows.h>
+#include <future>
+#include "ThreadManager.h"
+
+#include <winsock2.h>
+#include <mswsock.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+int main()
+{
+	// 윈도우 소켓 초기화 (ws2_32 라이브러리 초기화)
+	// 관련 정보가 wsaData에 채워짐
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
+
+	// ad : Address Family (AF_INET = IPv4, AF_INET6 = IPv6)
+	// type : TCP(SOCK_STREAM) vs UDP(SOCK_DGRAM)
+	// protocol : 0
+	// return : descriptor
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocket == INVALID_SOCKET)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Socket ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	// 나의 주소는? (IP주소 + Port)->XX 아파트 YY 호
+	SOCKADDR_IN serverAddr; // IPv4
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY); // 니가 알아서 골라줘
+	serverAddr.sin_port = ::htons(7777); // 80 : HTTP
+
+	// 안내원 폰 개통! 식당의 대표 번호
+	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Bind ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	// 영업 시작!
+	if (::listen(listenSocket, 10) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Listen ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	// -----------------------------
+
+	while (true)
+	{
+		SOCKADDR_IN clientAddr; // IPv4
+		::memset(&clientAddr, 0, sizeof(clientAddr));
+		int32 addrLen = sizeof(clientAddr);
+
+		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+		if (clientSocket == INVALID_SOCKET)
+		{
+			int32 errCode = ::WSAGetLastError();
+			cout << "Accept ErrorCode : " << errCode << endl;
+			return 0;
+		}
+
+		// 손님 입장!
+		char ipAddress[16];
+		::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddress, sizeof(ipAddress)); // ip address를 문자열로 바꿔주는 함수
+		cout << "Client Connected! IP = " << ipAddress << endl;
+
+		// TODO
+	}
+
+	// -----------------------------
+
+
+	// 윈속 종료
+	::WSACleanup();
+}
