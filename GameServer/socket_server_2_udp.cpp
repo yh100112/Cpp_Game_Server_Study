73194b356e9707d1ae52661630e370c9e5d73717
김덕용 <yh100112@naver.com>
2024-10-29 23:51:51 +0900

    소켓 옵션 지정

--- GameServer/socket_server_2_udp.cpp
+++ GameServer/socket_server_2_udp.cpp
@@ -74,6 +74,64 @@ int main()
 	}
 
 
+	// 옵션을 해석하고 처리할 주체
+	// 소켓 코드 -> SQL_SOCKET
+	// IPv4 -> IPPROTO_IP
+	// TCP 프로토콜 -> IPPROTO_TCP
+
+	// SO_KEEPALIVE = 주기적으로 연결 상태 확인 여부 ( TCP Only )
+	// 상대방이 소리소문없이 연결 끊는다면?
+	// 주기적으로 TCP 프로토콜 연결 상태 확인 -> 끊어진 연결 감지
+	bool enable = true;
+	::setsockopt(serverSocket, SOL_SOCKET, SO_KEEPALIVE, (char*)&enable, sizeof(enable));
+
+	// SO_LINGER = 지연하다.
+	// 송신 버퍼에 있는 데이터를 보낼 것인가? 날릴 것인가?
+	// onoff = 0이면 closesocket()이 바로 리턴. 아니면 linger초만큼 대기 (default 0)
+	// linger : 대기 시간
+	LINGER linger;
+	linger.l_onoff = 1;
+	linger.l_linger = 5;
+	::setsockopt(serverSocket, SOL_SOCKET, SO_KEEPALIVE, (char*)&linger, sizeof(linger));
+
+	// half-close
+	// SD_SEND : send 막는다.
+	// SD_RECEIVE : recv 막는다
+	// SD_BOTH : 둘 다 막는다
+	::shutdown(serverSocket, SD_SEND);
+
+	// 소켓 리소스 반환 ( 소켓 닫기 )
+	// send -> closesocket
+	::closesocket(serverSocket);
+
+	// SO_SNDBUF = 송신 버퍼 크기
+	// SO_RCVBUF = 수신 버퍼 크기
+	int32 sendBufferSize;
+	int32 optionLen = sizeof(sendBufferSize);
+	::getsockopt(serverSocket, SOL_SOCKET, SO_SNDBUF, (char*)&sendBufferSize, &optionLen);
+	cout << "송신 버퍼 크기 : " << sendBufferSize << endl;
+	int32 recvBufferSize;
+	int32 optionLen = sizeof(recvBufferSize);
+	::getsockopt(serverSocket, SOL_SOCKET, SO_RCVBUF, (char*)&recvBufferSize, &optionLen);
+	cout << "수신 버퍼 크기 : " << recvBufferSize << endl;
+
+	// SO_REUSEADDR
+	// ip 주소 및 port 재사용 ( 강제로 내가 그 port를 지금 사용하겠다! )
+	{
+		::setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, (char*)&enable, sizeof(enable));
+	}
+
+	// IPPROTO_TCP
+	// TCP_NODELAY = Nagle 알고리즘 작동 여부
+	// 데이터가 충분히 크면 보내고, 그렇지 않으면 데이터가 충분히 쌓일 때까지 대기!
+	// 장점 : 작은 패킷이 불필요하게 많이 생성되는 일을 방지
+	// 단점 : 반응 시간 손해
+	// 게임에서는 일반적으로 이 옵션은 끔
+	{
+		bool enable = true;
+		::setsockopt(serverSocket, IPPROTO_TCP, TCP_NODELAY, (char*)&enable, sizeof(enable));
+	}
+
 	// 윈속 종료
 	::WSACleanup();
 }
