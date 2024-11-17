d01ed01833117af0fd7f8667188675484262f8d2
김덕용 <yh100112@naver.com>
2024-11-17 17:36:31 +0900

    Send Buffer Pooling

--- DummyClient/send_recv_buffer_session_client.cpp
+++ DummyClient/send_recv_buffer_session_client.cpp
@@ -1,8 +1,7 @@
 #include "pch.h"
-#include <iostream>
 #include "ThreadManager.h"
 #include "Service.h"
-#include "Session.h"	
+#include "Session.h"
 
 char sendData[] = "Hello World";
 
@@ -13,12 +12,15 @@ public:
 	{
 		cout << "~ServerSession" << endl;
 	}
+
 	virtual void OnConnected() override
 	{
 		cout << "Connected To Server" << endl;
 
-		SendBufferRef sendBuffer = MakeShared<SendBuffer>(4096);
-		sendBuffer->CopyData(sendData, sizeof(sendData));
+		SendBufferRef sendBuffer = GSendBufferManager->Open(4096);
+		::memcpy(sendBuffer->Buffer(), sendData, sizeof(sendData));
+		sendBuffer->Close(sizeof(sendData));
+
 		Send(sendBuffer);
 	}
 
@@ -28,8 +30,10 @@ public:
 
 		this_thread::sleep_for(1s);
 
-		SendBufferRef sendBuffer = MakeShared<SendBuffer>(4096);
-		sendBuffer->CopyData(sendData, sizeof(sendData));
+		SendBufferRef sendBuffer = GSendBufferManager->Open(4096);
+		::memcpy(sendBuffer->Buffer(), sendData, sizeof(sendData));
+		sendBuffer->Close(sizeof(sendData));
+
 		Send(sendBuffer);
 
 		return len;
@@ -53,19 +57,18 @@ int main()
 	ClientServiceRef service = MakeShared<ClientService>(
 		NetAddress(L"127.0.0.1", 7777),
 		MakeShared<IocpCore>(),
-		MakeShared<ServerSession>,
-		5 // client 5명
-	);
+		MakeShared<ServerSession>, // TODO : SessionManager 등
+		5);
 
 	ASSERT_CRASH(service->Start());
 
-	for (int32 i = 0; i < 5; i++)
+	for (int32 i = 0; i < 2; i++)
 	{
 		GThreadManager->Launch([=]()
 			{
 				while (true)
 				{
-					service->GetIocpCore()->Dispatch();  // iocp core에 들어온 일감을 감지하고 있다면 처리해준다.
+					service->GetIocpCore()->Dispatch();
 				}
 			});
 	}
--- ServerCore/CoreGlobal.cpp
+++ ServerCore/CoreGlobal.cpp
@@ -4,9 +4,12 @@
 #include "Memory.h"
 #include "DeadLockProfiler.h"
 #include "SocketUtils.h"
+#include "SendBuffer.h"
 
 ThreadManager*		GThreadManager = nullptr;
 Memory*				GMemory = nullptr;
+SendBufferManager*	GSendBufferManager = nullptr;
+
 DeadLockProfiler*	GDeadLockProfiler = nullptr;
 
 class CoreGlobal
@@ -16,6 +19,7 @@ public:
 	{
 		GThreadManager = new ThreadManager();
 		GMemory = new Memory();
+		GSendBufferManager = new SendBufferManager();
 		GDeadLockProfiler = new DeadLockProfiler();
 		SocketUtils::Init(); // 윈속 초기화를 여기서 해줌
 	}
@@ -23,6 +27,7 @@ public:
 	{
 		delete GThreadManager;
 		delete GMemory;
+		delete GSendBufferManager;
 		delete GDeadLockProfiler;
 		SocketUtils::Clear(); // init과 짝을 맞춤
 	}
