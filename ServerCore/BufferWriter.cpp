6782a5a1c51f3df4ff573e9617461c1567d53ad2
김덕용 <yh100112@naver.com>
2024-11-20 22:29:41 +0900

    패킷 직렬화 3

--- ServerCore/BufferWriter.cpp
+++ ServerCore/BufferWriter.cpp
@@ -1,20 +1,24 @@
 #include "pch.h"
 #include "BufferWriter.h"
 
-/*-----------------
+/*----------------
 	BufferWriter
 -----------------*/
+
 BufferWriter::BufferWriter()
 {
+
 }
 
 BufferWriter::BufferWriter(BYTE* buffer, uint32 size, uint32 pos)
 	: _buffer(buffer), _size(size), _pos(pos)
 {
+
 }
 
 BufferWriter::~BufferWriter()
 {
+
 }
 
 bool BufferWriter::Write(void* src, uint32 len)
