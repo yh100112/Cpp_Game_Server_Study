452a3317ff556b73d27d2dea60ea44eeecf248b4
김덕용 <yh100112@naver.com>
2025-01-09 23:04:42 +0900

    Job Queue 구현 - Job을 하나하나 새롭게 생길 때마다 클래스 하나씩 생성해서 추가 ( job은 main에서만 빼내서 처리함 )

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -8,9 +8,26 @@
 #include "ClientPacketHandler.h"
 #include <tchar.h>
 #include "Protocol.pb.h"
+#include "Job.h"
+#include "Room.h"
 
 int main()
 {
+	// TEST JOB
+	{
+		// [일감 의뢰 내용] : 1번 유저한테 10만큼 힐을 줘라!
+		// 행동 : Heal
+		// 인자 : 1번 유저, 10이라는 힐량
+		HealJob healJob;
+		healJob._target = 1;
+		healJob._healValue = 10;
+
+		// 나~중에
+		healJob.Execute();
+	}
+
+	// JOB
+
 	ClientPacketHandler::Init();
 
 	ServerServiceRef service = MakeShared<ServerService>(
@@ -31,6 +48,12 @@ int main()
 				}				
 			});
 	}	
+
+	while (true)
+	{
+		GRoom.FlushJob();
+		this_thread::sleep_for(1ms);
+	}
 
 	GThreadManager->Join();
 }
