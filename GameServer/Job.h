db67f52a7540cb482f037a930f8c2369045e7903
김덕용 <yh100112@naver.com>
2025-01-12 17:36:39 +0900

    DB Bind 간편하게 사용 가능하도록 리팩토링

--- GameServer/Job.h
+++ GameServer/Job.h
@@ -88,33 +88,3 @@ private:
 	FuncType	_func;
 	std::tuple<Args...> _tuple;
 };
-/// /////////////
-
-
-
-using JobRef = shared_ptr<IJob>;
-
-class JobQueue
-{
-public:
-	void Push(JobRef job)
-	{
-		WRITE_LOCK;
-		_jobs.push(job);		
-	}
-
-	JobRef Pop()
-	{
-		WRITE_LOCK;
-		if (_jobs.empty())
-			return nullptr;
-
-		JobRef ret = _jobs.front();
-		_jobs.pop();
-		return ret;
-	}
-
-private:
-	USE_LOCK;
-	queue<JobRef> _jobs;
-};
